cd24c78ede10ea3e7290507b26d639fa1628fea9
walter <walter@uci.cu>
2019-01-21 17:30:01 -0500

    Add padding to Content component

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Alert } from "react-bootstrap";
+import styled from "styled-components";
+
+const Content = styled.div`
+  padding: 2em;
+`;
 
 export default ({ extra }) => {
   const link = extra;
   return (
-    <Alert variant={"primary"}>
-      Este es el contenido "{link.slug}" y "{link.value}"
-    </Alert>
+    <Content>
+      <Alert variant={"primary"}>
+        Este es el contenido "{link.slug}" y "{link.value}"
+      </Alert>
+    </Content>
   );
 };
